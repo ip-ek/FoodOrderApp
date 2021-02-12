5b658d07ad36c0714ccaea9f8d9b0b6454a85480
ipek <cw_ipk@outlook.com>
2021-02-12 23:51:29 +0300

    Splash screen sdk version problem solved

--- app/src/main/java/com/ipk/foodorderapp/SplashScreenActivity.kt
+++ app/src/main/java/com/ipk/foodorderapp/SplashScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.ipk.foodorderapp
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.MediaController
@@ -10,30 +11,41 @@ import android.os.Handler
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            setContentView(R.layout.activity_splash_screen)
+            try {
+                videoView.setVideoPath("android.resource://" + packageName + "/raw/splash_screen") //set the path of the video that we need to use in our VideoView
+                videoView.setOnCompletionListener {
+                    val r = object:Runnable{
+                        override fun run() {
+                            startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
+                            finish()
+                        }
+                    }
+                    Handler().postDelayed(r,0) //delay to starting new activity
+                }
+                videoView.start()
 
-        try {
-            //set time in mili
-            //Thread.sleep(5000);
-
-            videoView.setVideoPath("android.resource://" + packageName + "/raw/splash_screen") //set the path of the video that we need to use in our VideoView
-            videoView.setOnCompletionListener {
-                val r = object:Runnable{
-                    override fun run() {
+            }catch (e: Exception){
+                e.printStackTrace()
+                startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
+                finish()
+            }
+        }else{
+            setContentView(R.layout.splash_screen_sdk)
+            val background = object : Thread() {
+                override fun run() {
+                    try {
+                        sleep(5000)
+                    }catch (e : Exception){
+                        e.printStackTrace()
+                    }finally {
                         startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
                         finish()
                     }
                 }
-                Handler().postDelayed(r,0) //delay to starting new activity
             }
-            videoView.start()
-
-        }catch (e: Exception){
-            e.printStackTrace();
-            startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
-            finish()
+            background.start()
         }
-
-
     }
 }
